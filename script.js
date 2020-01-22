ca571e1aecf2dd3d89ac4a61dec21b63311b922e
Artak Mikayelyan <miqart@mail.ru>
2020-01-22 21:19:32 +0400

    v0.9

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const headerLinks = document.querySelector(".header-links");
 const links = document.querySelectorAll(".header-links li");
+const header = document.getElementById("header");
 
 hamburger.addEventListener("click", () => {
   headerLinks.classList.toggle("open");
@@ -8,3 +9,14 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("small");
+  }
+  else {
+    header.classList.remove("small");
+  }
+}
